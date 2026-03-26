c3f87778f10640fccbf5a466783097f97c81937a
David Novák <david.novak@druit.org>
2026-03-26 20:04:59 +0100

    feat: detail page

--- src/data/flats.ts
+++ src/data/flats.ts
@@ -1,3 +1,29 @@
+import flat1 from "../assets/build/flats/1np_apt1.svg"
+import flat2 from "../assets/build/flats/1np_apt2.svg"
+import flat3 from "../assets/build/flats/1np_apt3.svg"
+import flat4 from "../assets/build/flats/1np_apt4.svg"
+import flat5 from "../assets/build/flats/1np_apt5.svg"
+import flat6 from "../assets/build/flats/1np_apt6.svg"
+import flat7 from "../assets/build/flats/1np_apt7.svg"
+import flat8 from "../assets/build/flats/1np_apt8.svg"
+import flat9 from "../assets/build/flats/1pp_apt9.svg"
+import flat10 from "../assets/build/flats/1pp_apt10.svg"
+import flat11 from "../assets/build/flats/1pp_apt11.svg"
+import flat12 from "../assets/build/flats/1pp_apt12.svg"
+
+import detail1 from "../assets/build/flats/Apt1.svg"
+import detail2 from "../assets/build/flats/Apt2.svg"
+import detail3 from "../assets/build/flats/Apt3.svg"
+import detail4 from "../assets/build/flats/Apt4.svg"
+import detail5 from "../assets/build/flats/Apt5.svg"
+import detail6 from "../assets/build/flats/Apt6.svg"
+import detail7 from "../assets/build/flats/Apt7.svg"
+import detail8 from "../assets/build/flats/Apt8.svg"
+import detail9 from "../assets/build/flats/Apt9.svg"
+import detail10 from "../assets/build/flats/Apt10.svg"
+import detail11 from "../assets/build/flats/Apt11.svg"
+import detail12 from "../assets/build/flats/Apt12.svg"
+
 export const flats = [
 		{
 		id: 1,
@@ -7,7 +33,12 @@ export const flats = [
 		cellar: "ano",
 		orientation: "J",
 		area: "76,0",
+		floorArea: "",
 		price: "Ústní rezervace",
+		floorPlan: flat1,
+		flatPlan: detail1,
+		state: "Fit out",
+		parking: "ano"
 	},
 	{
 		id: 2,
@@ -17,7 +48,12 @@ export const flats = [
 		cellar: "ano",
 		orientation: "J",
 		area: "110,5",
+		floorArea: "",
 		price: "Ústní rezervace",
+		floorPlan: flat2,
+		flatPlan: detail2,
+		state: "Fit out",
+		parking: "ano"
 	},
 	{
 		id: 3,
@@ -27,7 +63,12 @@ export const flats = [
 		cellar: "ano",
 		orientation: "J",
 		area: "109,4",
+		floorArea: "",
 		price: "Cena na dotaz",
+		floorPlan: flat3,
+		flatPlan: detail3,
+		state: "Fit out",
+		parking: "ano"
 	},
 	{
 		id: 4,
@@ -37,7 +78,12 @@ export const flats = [
 		cellar: "ano",
 		orientation: "J",
 		area: "74,5",
+		floorArea: "",
 		price: "Cena na dotaz",
+		floorPlan: flat4,
+		flatPlan: detail4,
+		state: "Fit out",
+		parking: "ano"
 	},
 	{
 		id: 5,
@@ -47,7 +93,12 @@ export const flats = [
 		cellar: "ano",
 		orientation: "J",
 		area: "74,9",
+		floorArea: "",
 		price: "Cena na dotaz",
+		floorPlan: flat5,
+		flatPlan: detail5,
+		state: "Fit out",
+		parking: "ano"
 	},
 	{
 		id: 6,
@@ -57,7 +108,12 @@ export const flats = [
 		cellar: "ano",
 		orientation: "J",
 		area: "109,6",
+		floorArea: "",
 		price: "Cena na dotaz",
+		floorPlan: flat6,
+		flatPlan: detail6,
+		state: "Fit out",
+		parking: "ano"
 	},
 	{
 		id: 7,
@@ -67,7 +123,12 @@ export const flats = [
 		cellar: "ano",
 		orientation: "J",
 		area: "107,7",
+		floorArea: "",
 		price: "Cena na dotaz",
+		floorPlan: flat7,
+		flatPlan: detail7,
+		state: "Fit out",
+		parking: "ano"
 	},
 	{
 		id: 8,
@@ -77,7 +138,12 @@ export const flats = [
 		cellar: "ano",
 		orientation: "J",
 		area: "72,2",
+		floorArea: "",
 		price: "Cena na dotaz",
+		floorPlan: flat8,
+		flatPlan: detail8,
+		state: "Fit out",
+		parking: "ano"
 	},
 	{
 		id: 9,
@@ -87,7 +153,12 @@ export const flats = [
 		cellar: "ano",
 		orientation: "J",
 		area: "55,6",
+		floorArea: "",
 		price: "Cena na dotaz",
+		floorPlan: flat9,
+		flatPlan: detail9,
+		state: "Fit out",
+		parking: "ano"
 	},
 	{
 		id: 10,
@@ -97,7 +168,12 @@ export const flats = [
 		cellar: "ano",
 		orientation: "J",
 		area: "42,7",
+		floorArea: "",
 		price: "Cena na dotaz",
+		floorPlan: flat10,
+		flatPlan: detail10,
+		state: "Fit out",
+		parking: "ano"
 	},
 	{
 		id: 11,
@@ -107,7 +183,12 @@ export const flats = [
 		cellar: "ano",
 		orientation: "J",
 		area: "83,7",
+		floorArea: "",
 		price: "Cena na dotaz",
+		floorPlan: flat11,
+		flatPlan: detail11,
+		state: "Fit out",
+		parking: "ano"
 	},
 	{
 		id: 12,
@@ -117,6 +198,11 @@ export const flats = [
 		cellar: "ano",
 		orientation: "J",
 		area: "??",
+		floorArea: "",
 		price: "Cena na dotaz",
+		floorPlan: flat12,
+		flatPlan: detail12,
+		state: "Fit out",
+		parking: "ano"
 	},
 ];
