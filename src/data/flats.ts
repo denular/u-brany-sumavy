6d0fedc10951c92b4f5e1dab8bc298ad551a4e64
David Novák <david.novak@druit.org>
2026-04-23 19:17:17 +0200

    feat: Flat gallery

--- src/data/flats.ts
+++ src/data/flats.ts
@@ -1,220 +1,273 @@
-import flat1 from "../assets/build/flats/1np_apt1.svg"
-import flat2 from "../assets/build/flats/1np_apt2.svg"
-import flat3 from "../assets/build/flats/1np_apt3.svg"
-import flat4 from "../assets/build/flats/1np_apt4.svg"
-import flat5 from "../assets/build/flats/1np_apt5.svg"
-import flat6 from "../assets/build/flats/1np_apt6.svg"
-import flat7 from "../assets/build/flats/1np_apt7.svg"
-import flat8 from "../assets/build/flats/1np_apt8.svg"
-import flat9 from "../assets/build/flats/1pp_apt9.svg"
-import flat10 from "../assets/build/flats/1pp_apt10.svg"
-import flat11 from "../assets/build/flats/1pp_apt11.svg"
-import flat12 from "../assets/build/flats/1pp_apt12.svg"
+// Floor plan
+import flat1 from "#assets/build/flats/1np_apt1.svg";
+import flat2 from "#assets/build/flats/1np_apt2.svg";
+import flat3 from "#assets/build/flats/1np_apt3.svg";
+import flat4 from "#assets/build/flats/1np_apt4.svg";
+import flat5 from "#assets/build/flats/1np_apt5.svg";
+import flat6 from "#assets/build/flats/1np_apt6.svg";
+import flat7 from "#assets/build/flats/1np_apt7.svg";
+import flat8 from "#assets/build/flats/1np_apt8.svg";
+import flat9 from "#assets/build/flats/1pp_apt9.svg";
+import flat10 from "#assets/build/flats/1pp_apt10.svg";
+import flat11 from "#assets/build/flats/1pp_apt11.svg";
+import flat12 from "#assets/build/flats/1pp_apt12.svg";
 
-import detail1 from "../assets/build/flats/Apt1.svg"
-import detail2 from "../assets/build/flats/Apt2.svg"
-import detail3 from "../assets/build/flats/Apt3.svg"
-import detail4 from "../assets/build/flats/Apt4.svg"
-import detail5 from "../assets/build/flats/Apt5.svg"
-import detail6 from "../assets/build/flats/Apt6.svg"
-import detail7 from "../assets/build/flats/Apt7.svg"
-import detail8 from "../assets/build/flats/Apt8.svg"
-import detail9 from "../assets/build/flats/Apt9.svg"
-import detail10 from "../assets/build/flats/Apt10.svg"
-import detail11 from "../assets/build/flats/Apt11.svg"
-import detail12 from "../assets/build/flats/Apt12.svg"
+// Flat plan
+import detail1 from "#assets/build/flats/Apt1.svg";
+import detail2 from "#assets/build/flats/Apt2.svg";
+import detail3 from "#assets/build/flats/Apt3.svg";
+import detail4 from "#assets/build/flats/Apt4.svg";
+import detail5 from "#assets/build/flats/Apt5.svg";
+import detail6 from "#assets/build/flats/Apt6.svg";
+import detail7 from "#assets/build/flats/Apt7.svg";
+import detail8 from "#assets/build/flats/Apt8.svg";
+import detail9 from "#assets/build/flats/Apt9.svg";
+import detail10 from "#assets/build/flats/Apt10.svg";
+import detail11 from "#assets/build/flats/Apt11.svg";
+import detail12 from "#assets/build/flats/Apt12.svg";
+
+// Gallery images
+import img1 from "#assets/Image_galllery_asset_placeholder_wide.jpg";
 
 export const flats = [
-		{
-		id: 1,
-		floor: "1NP + 2NP",
-		layout: "3+kk",
-		outside: "terasa/předzahrádka",
-		cellar: "ano",
-		orientation: "J",
-		area: "76,0",
-		floorArea: "",
-		price: "Ústní rezervace",
-		floorPlan: flat1,
-		flatPlan: detail1,
-		state: "Fit out",
-		parking: "ano",
-		card: "U_Brany_Sumavy_Karta_B1.pdf"
-	},
-	{
-		id: 2,
-		floor: "1NP + 2NP",
-		layout: "4+kk",
-		outside: "terasa/předzahrádka",
-		cellar: "ano",
-		orientation: "J",
-		area: "110,5",
-		floorArea: "",
-		price: "Ústní rezervace",
-		floorPlan: flat2,
-		flatPlan: detail2,
-		state: "Fit out",
-		parking: "ano",
-		card: "U_Brany_Sumavy_Karta_B2.pdf"
-	},
-	{
-		id: 3,
-		floor: "1NP + 2NP",
-		layout: "4+kk",
-		outside: "terasa/předzahrádka",
-		cellar: "ano",
-		orientation: "J",
-		area: "109,4",
-		floorArea: "",
-		price: "Cena na dotaz",
-		floorPlan: flat3,
-		flatPlan: detail3,
-		state: "Fit out",
-		parking: "ano",
-		card: "U_Brany_Sumavy_Karta_B3.pdf"
-	},
-	{
-		id: 4,
-		floor: "1NP + 2NP",
-		layout: "3+kk",
-		outside: "terasa/předzahrádka",
-		cellar: "ano",
-		orientation: "J",
-		area: "74,5",
-		floorArea: "",
-		price: "Cena na dotaz",
-		floorPlan: flat4,
-		flatPlan: detail4,
-		state: "Fit out",
-		parking: "ano",
-		card: "U_Brany_Sumavy_Karta_B4.pdf"
-	},
-	{
-		id: 5,
-		floor: "1NP + 2NP",
-		layout: "3+kk",
-		outside: "terasa/předzahrádka",
-		cellar: "ano",
-		orientation: "J",
-		area: "74,9",
-		floorArea: "",
-		price: "Cena na dotaz",
-		floorPlan: flat5,
-		flatPlan: detail5,
-		state: "Fit out",
-		parking: "ano",
-		card: "U_Brany_Sumavy_Karta_B5.pdf"
-	},
-	{
-		id: 6,
-		floor: "1NP + 2NP",
-		layout: "4+kk",
-		outside: "terasa/předzahrádka",
-		cellar: "ano",
-		orientation: "J",
-		area: "109,6",
-		floorArea: "",
-		price: "Cena na dotaz",
-		floorPlan: flat6,
-		flatPlan: detail6,
-		state: "Fit out",
-		parking: "ano",
-		card: "U_Brany_Sumavy_Karta_B6.pdf"
-	},
-	{
-		id: 7,
-		floor: "1NP + 2NP",
-		layout: "4+kk",
-		outside: "terasa/předzahrádka",
-		cellar: "ano",
-		orientation: "J",
-		area: "107,7",
-		floorArea: "",
-		price: "Cena na dotaz",
-		floorPlan: flat7,
-		flatPlan: detail7,
-		state: "Fit out",
-		parking: "ano",
-		card: "U_Brany_Sumavy_Karta_B7.pdf"
-	},
-	{
-		id: 8,
-		floor: "1NP + 2NP",
-		layout: "3+kk",
-		outside: "terasa/předzahrádka",
-		cellar: "ano",
-		orientation: "J",
-		area: "72,2",
-		floorArea: "",
-		price: "Cena na dotaz",
-		floorPlan: flat8,
-		flatPlan: detail8,
-		state: "Fit out",
-		parking: "ano",
-		card: "U_Brany_Sumavy_Karta_B8.pdf"
-	},
-	{
-		id: 9,
-		floor: "1PP",
-		layout: "2+kk",
-		outside: "",
-		cellar: "ano",
-		orientation: "J",
-		area: "55,6",
-		floorArea: "",
-		price: "Cena na dotaz",
-		floorPlan: flat9,
-		flatPlan: detail9,
-		state: "Fit out",
-		parking: "ano",
-		card: "U_Brany_Sumavy_Karta_B9.pdf"
-	},
-	{
-		id: 10,
-		floor: "1PP",
-		layout: "1+kk",
-		outside: "",
-		cellar: "ano",
-		orientation: "J",
-		area: "42,7",
-		floorArea: "",
-		price: "Cena na dotaz",
-		floorPlan: flat10,
-		flatPlan: detail10,
-		state: "Fit out",
-		parking: "ano",
-		card: "U_Brany_Sumavy_Karta_B10.pdf"
-	},
-	{
-		id: 11,
-		floor: "1PP",
-		layout: "2+kk",
-		outside: "",
-		cellar: "ano",
-		orientation: "J",
-		area: "83,7",
-		floorArea: "",
-		price: "Cena na dotaz",
-		floorPlan: flat11,
-		flatPlan: detail11,
-		state: "Fit out",
-		parking: "ano",
-		card: "U_Brany_Sumavy_Karta_B11.pdf"
-	},
-	{
-		id: 12,
-		floor: "1PP",
-		layout: "2+kk",
-		outside: "",
-		cellar: "ano",
-		orientation: "J",
-		area: "??",
-		floorArea: "",
-		price: "Cena na dotaz",
-		floorPlan: flat12,
-		flatPlan: detail12,
-		state: "Fit out",
-		parking: "ano",
-		card: "U_Brany_Sumavy_Karta_B12.pdf"
-	},
+  {
+    id: 1,
+    floor: "1NP + 2NP",
+    layout: "3+kk",
+    outside: "terasa/předzahrádka",
+    cellar: "ano",
+    orientation: "J",
+    area: "76,0",
+    floorArea: "",
+    price: "Ústní rezervace",
+    floorPlan: flat1,
+    flatPlan: detail1,
+    state: "Fit out",
+    parking: "ano",
+    card: "U_Brany_Sumavy_Karta_B1.pdf",
+    gallery: [
+      { src: img1, alt: "Obrázek 1" },
+      { src: img1, alt: "Obrázek 2" },
+    ],
+  },
+  {
+    id: 2,
+    floor: "1NP + 2NP",
+    layout: "4+kk",
+    outside: "terasa/předzahrádka",
+    cellar: "ano",
+    orientation: "J",
+    area: "110,5",
+    floorArea: "",
+    price: "Ústní rezervace",
+    floorPlan: flat2,
+    flatPlan: detail2,
+    state: "Fit out",
+    parking: "ano",
+    card: "U_Brany_Sumavy_Karta_B2.pdf",
+    gallery: [
+      { src: img1, alt: "Obrázek 1" },
+      { src: img1, alt: "Obrázek 2" },
+    ],
+  },
+  {
+    id: 3,
+    floor: "1NP + 2NP",
+    layout: "4+kk",
+    outside: "terasa/předzahrádka",
+    cellar: "ano",
+    orientation: "J",
+    area: "109,4",
+    floorArea: "",
+    price: "Cena na dotaz",
+    floorPlan: flat3,
+    flatPlan: detail3,
+    state: "Fit out",
+    parking: "ano",
+    card: "U_Brany_Sumavy_Karta_B3.pdf",
+    gallery: [
+      { src: img1, alt: "Obrázek 1" },
+      { src: img1, alt: "Obrázek 2" },
+    ],
+  },
+  {
+    id: 4,
+    floor: "1NP + 2NP",
+    layout: "3+kk",
+    outside: "terasa/předzahrádka",
+    cellar: "ano",
+    orientation: "J",
+    area: "74,5",
+    floorArea: "",
+    price: "Cena na dotaz",
+    floorPlan: flat4,
+    flatPlan: detail4,
+    state: "Fit out",
+    parking: "ano",
+    card: "U_Brany_Sumavy_Karta_B4.pdf",
+    gallery: [
+      { src: img1, alt: "Obrázek 1" },
+      { src: img1, alt: "Obrázek 2" },
+    ],
+  },
+  {
+    id: 5,
+    floor: "1NP + 2NP",
+    layout: "3+kk",
+    outside: "terasa/předzahrádka",
+    cellar: "ano",
+    orientation: "J",
+    area: "74,9",
+    floorArea: "",
+    price: "Cena na dotaz",
+    floorPlan: flat5,
+    flatPlan: detail5,
+    state: "Fit out",
+    parking: "ano",
+    card: "U_Brany_Sumavy_Karta_B5.pdf",
+    gallery: [
+      { src: img1, alt: "Obrázek 1" },
+      { src: img1, alt: "Obrázek 2" },
+    ],
+  },
+  {
+    id: 6,
+    floor: "1NP + 2NP",
+    layout: "4+kk",
+    outside: "terasa/předzahrádka",
+    cellar: "ano",
+    orientation: "J",
+    area: "109,6",
+    floorArea: "",
+    price: "Cena na dotaz",
+    floorPlan: flat6,
+    flatPlan: detail6,
+    state: "Fit out",
+    parking: "ano",
+    card: "U_Brany_Sumavy_Karta_B6.pdf",
+    gallery: [
+      { src: img1, alt: "Obrázek 1" },
+      { src: img1, alt: "Obrázek 2" },
+    ],
+  },
+  {
+    id: 7,
+    floor: "1NP + 2NP",
+    layout: "4+kk",
+    outside: "terasa/předzahrádka",
+    cellar: "ano",
+    orientation: "J",
+    area: "107,7",
+    floorArea: "",
+    price: "Cena na dotaz",
+    floorPlan: flat7,
+    flatPlan: detail7,
+    state: "Fit out",
+    parking: "ano",
+    card: "U_Brany_Sumavy_Karta_B7.pdf",
+    gallery: [
+      { src: img1, alt: "Obrázek 1" },
+      { src: img1, alt: "Obrázek 2" },
+    ],
+  },
+  {
+    id: 8,
+    floor: "1NP + 2NP",
+    layout: "3+kk",
+    outside: "terasa/předzahrádka",
+    cellar: "ano",
+    orientation: "J",
+    area: "72,2",
+    floorArea: "",
+    price: "Cena na dotaz",
+    floorPlan: flat8,
+    flatPlan: detail8,
+    state: "Fit out",
+    parking: "ano",
+    card: "U_Brany_Sumavy_Karta_B8.pdf",
+    gallery: [
+      { src: img1, alt: "Obrázek 1" },
+      { src: img1, alt: "Obrázek 2" },
+    ],
+  },
+  {
+    id: 9,
+    floor: "1PP",
+    layout: "2+kk",
+    outside: "",
+    cellar: "ano",
+    orientation: "J",
+    area: "55,6",
+    floorArea: "",
+    price: "Cena na dotaz",
+    floorPlan: flat9,
+    flatPlan: detail9,
+    state: "Fit out",
+    parking: "ano",
+    card: "U_Brany_Sumavy_Karta_B9.pdf",
+    gallery: [
+      { src: img1, alt: "Obrázek 1" },
+      { src: img1, alt: "Obrázek 2" },
+    ],
+  },
+  {
+    id: 10,
+    floor: "1PP",
+    layout: "1+kk",
+    outside: "",
+    cellar: "ano",
+    orientation: "J",
+    area: "42,7",
+    floorArea: "",
+    price: "Cena na dotaz",
+    floorPlan: flat10,
+    flatPlan: detail10,
+    state: "Fit out",
+    parking: "ano",
+    card: "U_Brany_Sumavy_Karta_B10.pdf",
+    gallery: [
+      { src: img1, alt: "Obrázek 1" },
+      { src: img1, alt: "Obrázek 2" },
+    ],
+  },
+  {
+    id: 11,
+    floor: "1PP",
+    layout: "2+kk",
+    outside: "",
+    cellar: "ano",
+    orientation: "J",
+    area: "83,7",
+    floorArea: "",
+    price: "Cena na dotaz",
+    floorPlan: flat11,
+    flatPlan: detail11,
+    state: "Fit out",
+    parking: "ano",
+    card: "U_Brany_Sumavy_Karta_B11.pdf",
+    gallery: [
+      { src: img1, alt: "Obrázek 1" },
+      { src: img1, alt: "Obrázek 2" },
+    ],
+  },
+  {
+    id: 12,
+    floor: "1PP",
+    layout: "2+kk",
+    outside: "",
+    cellar: "ano",
+    orientation: "J",
+    area: "??",
+    floorArea: "",
+    price: "Cena na dotaz",
+    floorPlan: flat12,
+    flatPlan: detail12,
+    state: "Fit out",
+    parking: "ano",
+    card: "U_Brany_Sumavy_Karta_B12.pdf",
+    gallery: [
+      { src: img1, alt: "Obrázek 1" },
+      { src: img1, alt: "Obrázek 2" },
+    ],
+  },
 ];
