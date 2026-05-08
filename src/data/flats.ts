cba753147066d6a03c71824351de9f9ba30d2199
David Novák <david.novak@druit.org>
2026-05-08 17:05:56 +0200

    feat: apartments plans

--- src/data/flats.ts
+++ src/data/flats.ts
@@ -1,33 +1,28 @@
-// Floor plan
-import flat1 from '#assets/build/flats/1np_apt1.svg'
-import flat2 from '#assets/build/flats/1np_apt2.svg'
-import flat3 from '#assets/build/flats/1np_apt3.svg'
-import flat4 from '#assets/build/flats/1np_apt4.svg'
-import flat5 from '#assets/build/flats/1np_apt5.svg'
-import flat6 from '#assets/build/flats/1np_apt6.svg'
-import flat7 from '#assets/build/flats/1np_apt7.svg'
-import flat8 from '#assets/build/flats/1np_apt8.svg'
-import flat9 from '#assets/build/flats/1pp_apt9.svg'
-import flat10 from '#assets/build/flats/1pp_apt10.svg'
-import flat11 from '#assets/build/flats/1pp_apt11.svg'
-import flat12 from '#assets/build/flats/1pp_apt12.svg'
-
-// Flat plan
-import detail1 from '#assets/build/flats/Apt1.svg'
-import detail2 from '#assets/build/flats/Apt2.svg'
-import detail3 from '#assets/build/flats/Apt3.svg'
-import detail4 from '#assets/build/flats/Apt4.svg'
-import detail5 from '#assets/build/flats/Apt5.svg'
-import detail6 from '#assets/build/flats/Apt6.svg'
-import detail7 from '#assets/build/flats/Apt7.svg'
-import detail8 from '#assets/build/flats/Apt8.svg'
-import detail9 from '#assets/build/flats/Apt9.svg'
-import detail10 from '#assets/build/flats/Apt10.svg'
-import detail11 from '#assets/build/flats/Apt11.svg'
-import detail12 from '#assets/build/flats/Apt12.svg'
-
-// Gallery images
-import img1 from '#assets/Image_galllery_asset_placeholder_wide.jpg'
+// Apartments plans
+import apartments_plan_1_1 from '#assets/apartments_plans/Apartments_plans_1_1.png'
+import apartments_plan_1_2 from '#assets/apartments_plans/Apartments_plans_1_2.png'
+import apartments_plan_2_1 from '#assets/apartments_plans/Apartments_plans_2_1.png'
+import apartments_plan_2_2 from '#assets/apartments_plans/Apartments_plans_2_2.png'
+import apartments_plan_3_1 from '#assets/apartments_plans/Apartments_plans_3_1.png'
+import apartments_plan_3_2 from '#assets/apartments_plans/Apartments_plans_3_2.png'
+import apartments_plan_4_1 from '#assets/apartments_plans/Apartments_plans_4_1.png'
+import apartments_plan_4_2 from '#assets/apartments_plans/Apartments_plans_4_2.png'
+import apartments_plan_5_1 from '#assets/apartments_plans/Apartments_plans_5_1.png'
+import apartments_plan_5_2 from '#assets/apartments_plans/Apartments_plans_5_2.png'
+import apartments_plan_6_1 from '#assets/apartments_plans/Apartments_plans_6_1.png'
+import apartments_plan_6_2 from '#assets/apartments_plans/Apartments_plans_6_2.png'
+import apartments_plan_7_1 from '#assets/apartments_plans/Apartments_plans_7_1.png'
+import apartments_plan_7_2 from '#assets/apartments_plans/Apartments_plans_7_2.png'
+import apartments_plan_8_1 from '#assets/apartments_plans/Apartments_plans_8_1.png'
+import apartments_plan_8_2 from '#assets/apartments_plans/Apartments_plans_8_2.png'
+import apartments_plan_9_1 from '#assets/apartments_plans/Apartments_plans_9_1.png'
+import apartments_plan_9_2 from '#assets/apartments_plans/Apartments_plans_9_2.png'
+import apartments_plan_10_1 from '#assets/apartments_plans/Apartments_plans_10_1.png'
+import apartments_plan_10_2 from '#assets/apartments_plans/Apartments_plans_10_2.png'
+import apartments_plan_11_1 from '#assets/apartments_plans/Apartments_plans_11_1.png'
+import apartments_plan_11_2 from '#assets/apartments_plans/Apartments_plans_11_2.png'
+import apartments_plan_12_1 from '#assets/apartments_plans/Apartments_plans_12_1.png'
+import apartments_plan_12_2 from '#assets/apartments_plans/Apartments_plans_12_2.png'
 
 export const flats = [
 	{
@@ -40,15 +35,11 @@ export const flats = [
 		area: '76,0',
 		floorArea: '',
 		price: 'Cena na dotaz',
-		floorPlan: flat1,
-		flatPlan: detail1,
 		state: 'Fit out',
 		parking: 'ano',
 		card: 'U_Brany_Sumavy_Karta_B1.pdf',
-		gallery: [
-			{ src: img1, alt: 'Obrázek 1' },
-			{ src: img1, alt: 'Obrázek 2' },
-		],
+		infoCard: apartments_plan_1_1,
+		floorCard: apartments_plan_1_2,
 	},
 	{
 		id: 2,
@@ -60,15 +51,11 @@ export const flats = [
 		area: '110,5',
 		floorArea: '',
 		price: 'Cena na dotaz',
-		floorPlan: flat2,
-		flatPlan: detail2,
 		state: 'Fit out',
 		parking: 'ano',
 		card: 'U_Brany_Sumavy_Karta_B2.pdf',
-		gallery: [
-			{ src: img1, alt: 'Obrázek 1' },
-			{ src: img1, alt: 'Obrázek 2' },
-		],
+		infoCard: apartments_plan_2_1,
+		floorCard: apartments_plan_2_2,
 	},
 	{
 		id: 3,
@@ -80,15 +67,11 @@ export const flats = [
 		area: '109,4',
 		floorArea: '',
 		price: 'Cena na dotaz',
-		floorPlan: flat3,
-		flatPlan: detail3,
 		state: 'Fit out',
 		parking: 'ano',
 		card: 'U_Brany_Sumavy_Karta_B3.pdf',
-		gallery: [
-			{ src: img1, alt: 'Obrázek 1' },
-			{ src: img1, alt: 'Obrázek 2' },
-		],
+		infoCard: apartments_plan_3_1,
+		floorCard: apartments_plan_3_2,
 	},
 	{
 		id: 4,
@@ -100,15 +83,11 @@ export const flats = [
 		area: '74,5',
 		floorArea: '',
 		price: 'Cena na dotaz',
-		floorPlan: flat4,
-		flatPlan: detail4,
 		state: 'Fit out',
 		parking: 'ano',
 		card: 'U_Brany_Sumavy_Karta_B4.pdf',
-		gallery: [
-			{ src: img1, alt: 'Obrázek 1' },
-			{ src: img1, alt: 'Obrázek 2' },
-		],
+		infoCard: apartments_plan_4_1,
+		floorCard: apartments_plan_4_2,
 	},
 	{
 		id: 5,
@@ -120,15 +99,11 @@ export const flats = [
 		area: '74,9',
 		floorArea: '',
 		price: 'Cena na dotaz',
-		floorPlan: flat5,
-		flatPlan: detail5,
 		state: 'Fit out',
 		parking: 'ano',
 		card: 'U_Brany_Sumavy_Karta_B5.pdf',
-		gallery: [
-			{ src: img1, alt: 'Obrázek 1' },
-			{ src: img1, alt: 'Obrázek 2' },
-		],
+		infoCard: apartments_plan_5_1,
+		floorCard: apartments_plan_5_2,
 	},
 	{
 		id: 6,
@@ -140,15 +115,11 @@ export const flats = [
 		area: '109,6',
 		floorArea: '',
 		price: 'Cena na dotaz',
-		floorPlan: flat6,
-		flatPlan: detail6,
 		state: 'Fit out',
 		parking: 'ano',
 		card: 'U_Brany_Sumavy_Karta_B6.pdf',
-		gallery: [
-			{ src: img1, alt: 'Obrázek 1' },
-			{ src: img1, alt: 'Obrázek 2' },
-		],
+		infoCard: apartments_plan_6_1,
+		floorCard: apartments_plan_6_2,
 	},
 	{
 		id: 7,
@@ -160,15 +131,11 @@ export const flats = [
 		area: '107,7',
 		floorArea: '',
 		price: 'Cena na dotaz',
-		floorPlan: flat7,
-		flatPlan: detail7,
 		state: 'Fit out',
 		parking: 'ano',
 		card: 'U_Brany_Sumavy_Karta_B7.pdf',
-		gallery: [
-			{ src: img1, alt: 'Obrázek 1' },
-			{ src: img1, alt: 'Obrázek 2' },
-		],
+		infoCard: apartments_plan_7_1,
+		floorCard: apartments_plan_7_2,
 	},
 	{
 		id: 8,
@@ -180,15 +147,11 @@ export const flats = [
 		area: '72,2',
 		floorArea: '',
 		price: 'Cena na dotaz',
-		floorPlan: flat8,
-		flatPlan: detail8,
 		state: 'Fit out',
 		parking: 'ano',
 		card: 'U_Brany_Sumavy_Karta_B8.pdf',
-		gallery: [
-			{ src: img1, alt: 'Obrázek 1' },
-			{ src: img1, alt: 'Obrázek 2' },
-		],
+		infoCard: apartments_plan_8_1,
+		floorCard: apartments_plan_8_2,
 	},
 	{
 		id: 9,
@@ -200,15 +163,11 @@ export const flats = [
 		area: '55,6',
 		floorArea: '',
 		price: 'Cena na dotaz',
-		floorPlan: flat9,
-		flatPlan: detail9,
 		state: 'Fit out',
 		parking: 'ano',
 		card: 'U_Brany_Sumavy_Karta_B9.pdf',
-		gallery: [
-			{ src: img1, alt: 'Obrázek 1' },
-			{ src: img1, alt: 'Obrázek 2' },
-		],
+		infoCard: apartments_plan_9_1,
+		floorCard: apartments_plan_9_2,
 	},
 	{
 		id: 10,
@@ -220,15 +179,11 @@ export const flats = [
 		area: '42,7',
 		floorArea: '',
 		price: 'Cena na dotaz',
-		floorPlan: flat10,
-		flatPlan: detail10,
 		state: 'Fit out',
 		parking: 'ano',
 		card: 'U_Brany_Sumavy_Karta_B10.pdf',
-		gallery: [
-			{ src: img1, alt: 'Obrázek 1' },
-			{ src: img1, alt: 'Obrázek 2' },
-		],
+		infoCard: apartments_plan_10_1,
+		floorCard: apartments_plan_10_2,
 	},
 	{
 		id: 11,
@@ -240,15 +195,11 @@ export const flats = [
 		area: '83,7',
 		floorArea: '',
 		price: 'Cena na dotaz',
-		floorPlan: flat11,
-		flatPlan: detail11,
 		state: 'Fit out',
 		parking: 'ano',
 		card: 'U_Brany_Sumavy_Karta_B11.pdf',
-		gallery: [
-			{ src: img1, alt: 'Obrázek 1' },
-			{ src: img1, alt: 'Obrázek 2' },
-		],
+		infoCard: apartments_plan_11_1,
+		floorCard: apartments_plan_11_2,
 	},
 	{
 		id: 12,
@@ -260,14 +211,10 @@ export const flats = [
 		area: '??',
 		floorArea: '',
 		price: 'Cena na dotaz',
-		floorPlan: flat12,
-		flatPlan: detail12,
 		state: 'Fit out',
 		parking: 'ano',
 		card: 'U_Brany_Sumavy_Karta_B12.pdf',
-		gallery: [
-			{ src: img1, alt: 'Obrázek 1' },
-			{ src: img1, alt: 'Obrázek 2' },
-		],
+		infoCard: apartments_plan_12_1,
+		floorCard: apartments_plan_12_2,
 	},
 ]
---
+++ src/types/arguments.ts
@@ -0,0 +1,10 @@
+export interface ArgumentContent {
+	text: string
+}
+
+export interface ArgumentsData {
+	about: ArgumentContent
+	aboutLocality: ArgumentContent
+	locality: ArgumentContent
+	investment: ArgumentContent
+}
